34ffde529e943e03caa37ffbd13e29347fcb07ef
Filip Pietruszyński <filippietruszynski@pm.me>
2021-08-09 18:49:08 +0200

    Setup project

--- webpack.dev.config.ts
+++ webpack.dev.config.ts
@@ -42,6 +42,7 @@ const config: Configuration = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
+      favicon: "src/favicon.ico",
     }),
     new HotModuleReplacementPlugin(),
     new ForkTsCheckerWebpackPlugin({
--- webpack.prod.config.ts
+++ webpack.prod.config.ts
@@ -37,6 +37,7 @@ const config: webpack.Configuration = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
+      favicon: "src/favicon.ico",
     }),
     new ForkTsCheckerWebpackPlugin({
       async: false,
