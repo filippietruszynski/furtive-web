4a07a087c18c632904975b874c729f428871806e
Filip Pietruszyński <filippietruszynski@protonmail.com>
2021-09-01 22:53:03 +0200

    Add ReturnType to api request functions

--- src/store/user/api.ts
+++ src/store/user/api.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import axiosInstance from "../../utils/axios-instance";
 
 import {
@@ -6,10 +7,14 @@ import {
   ILogInUserResponse,
 } from "./types";
 
-export const signUpUserRequest = (request: ISignUpUserRequest) => {
+export const signUpUserRequest = (
+  request: ISignUpUserRequest
+): ReturnType<typeof axios.post> => {
   return axiosInstance.post("/signup", request);
 };
 
-export const logInUserRequest = (request: ILogInUserRequest) => {
+export const logInUserRequest = (
+  request: ILogInUserRequest
+): ReturnType<typeof axios.post> => {
   return axiosInstance.post<ILogInUserResponse>("/login", request);
 };
