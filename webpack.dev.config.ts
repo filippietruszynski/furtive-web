038a933b9a2f340dce201d4b4fdc10bf1a33bf41
Filip Pietruszyński <filippietruszynski@protonmail.com>
2021-09-02 19:33:33 +0200

    Remove favicon

--- webpack.dev.config.ts
+++ webpack.dev.config.ts
@@ -42,7 +42,6 @@ const config: Configuration = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
-      favicon: "src/favicon.ico",
     }),
     new HotModuleReplacementPlugin(),
     new ForkTsCheckerWebpackPlugin({
