68acf5d816bd9468c8554f3bc0c12ecf512464f7
Filip Pietruszyński <filippietruszynski@protonmail.com>
2021-09-09 21:24:04 +0200

    Clean App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,13 @@
-import React, { useState } from "react";
-import { BrowserRouter, Switch } from "react-router-dom";
+import React from "react";
 
-import DummyPrivateView from "./components/DummyPrivateView";
 import LogIn from "./components/LogIn";
-import PrivateRoute from "./components/PrivateRoute";
-
-import { IPrivateRouteProps } from "./components/PrivateRoute/PrivateRoute";
 import SignUp from "./components/SignUp";
 
 const App: React.FC = () => {
-  // const defaultPrivateRouteProps: IPrivateRouteProps = {
-  //   isLoggedIn: isLoggedIn,
-  //   logInPath: "/login",
-  //   redirectPath: redirectPath,
-  //   setRedirectPath: setRedirectPath,
-  // };
-
   return (
     <>
-      {process.env.SZOT}
       <LogIn />
       <SignUp />
-
-      {/* <BrowserRouter>
-        <Switch>
-          <PrivateRoute
-            {...defaultPrivateRouteProps}
-            path="/dpv"
-            component={DummyPrivateView}
-            exact
-          />
-        </Switch>
-      </BrowserRouter> */}
     </>
   );
 };
