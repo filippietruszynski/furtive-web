60b54b9927288facbd49b33f1b43f72b0bbeb442
Filip Pietruszyński <filippietruszynski@protonmail.com>
2021-09-13 03:50:42 +0200

    Remove console.log

--- src/utils/axios-instance.ts
+++ src/utils/axios-instance.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import config from "../config";
 
 const colon: string = config.server.hasPort ? ":" : "";
-console.log(config);
+
 const axiosInstance = axios.create({
   baseURL: `https://${config.server.host}${colon}${config.server.port}/`,
 });
